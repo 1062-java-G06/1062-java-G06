5b5ff56c7274a0db4737f31926f323b78ef0c571
william1201 <39294956+william1201@users.noreply.github.com>
2018-06-07 17:15:52 +0800

    Add files via upload

---
+++ FireBall.java
@@ -0,0 +1,109 @@
+package Entity;
+
+import TileMap.TileMap;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class FireBall extends MapObject {
+	
+	private boolean hit;
+	private boolean remove;
+	private BufferedImage[] sprites;
+	private BufferedImage[] hitSprites;
+	
+	public FireBall(TileMap tm, boolean right) {
+		
+		super(tm);
+		
+		facingRight = right;
+		
+		moveSpeed = 2;
+		if(right) dx = moveSpeed;
+		else dx = -moveSpeed;
+		
+		width = 30;
+		height =30;
+		cwidth = 15;
+		cheight = 15;
+		
+		// load sprites
+		try {
+			
+			BufferedImage spritesheet = ImageIO.read(
+				getClass().getResourceAsStream(
+					"/Sprites/Player/FireBall1.png"
+				)
+			);
+			
+			sprites = new BufferedImage[4];
+			for(int i = 0; i < sprites.length; i++) {
+				sprites[i] = spritesheet.getSubimage(
+					i * width,
+					0,
+					width,
+					height
+				);
+			}
+			
+			hitSprites = new BufferedImage[3];
+			for(int i = 0; i < hitSprites.length; i++) {
+				hitSprites[i] = spritesheet.getSubimage(
+					i * width,
+					height,
+					width,
+					height
+				);
+			}
+			
+			animation = new Animation();
+			animation.setFrames(sprites);
+			animation.setDelay(150);
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void setHit() {
+		if(hit) return;
+		hit = true;
+		animation.setFrames(hitSprites);
+		animation.setDelay(150);
+		dx = 0;
+	}
+	
+	public boolean shouldRemove() { return remove; }
+	
+	public void update() {
+		
+		checkTileMapCollision();
+		setPosition(xtemp, ytemp);
+		
+		if(dx == 0 && !hit) {
+			setHit();
+		}
+		
+		animation.update();
+		if(hit && animation.hasPlayedOnce()) {
+			remove = true;
+		}
+		
+	}
+	
+	public void draw(Graphics2D g) {
+		
+		setMapPosition();
+		
+		super.draw(g);
+		
+	}
+	
+}
+
+
+
+
