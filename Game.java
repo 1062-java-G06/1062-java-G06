d503741064e4686c733718ba0aad3ad2a1da9505
jrl0421 <39295037+jrl0421@users.noreply.github.com>
2018-06-07 16:47:41 +0800

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Game {
+	
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame("小朋友走路");
+		window.setContentPane(new GamePanel());
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.pack();
+		window.setVisible(true);
+		
+	}
+	
+}
