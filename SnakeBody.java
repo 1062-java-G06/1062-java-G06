35593a6e7d3c70216ee50b77eb00579a07f8948b
william1201 <39294956+william1201@users.noreply.github.com>
2018-06-11 18:18:57 +0800

    Add files via upload

---
+++ SnakeBody.java
@@ -0,0 +1,51 @@
+package Snake;
+
+import java.awt.Color;
+
+public class SnakeBody {
+	
+    private int x;
+    private int y;
+    private Color color;
+    
+    public SnakeBody() {
+    	
+        super();
+
+    }
+    
+    public SnakeBody(int x, int y, Color color) {
+    	
+        super();
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
+    
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+
+
+}
